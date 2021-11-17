32d392073ad0b9f233619747de85798e45b11090
moomle00000 <68758566+moomle00000@users.noreply.github.com>
2021-11-17 18:56:01 +0300

    Update moovie.js

--- moovie.js
+++ moovie.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", function() {
 var video = demo.video;
 if (Hls.isSupported()) {
   var hls = new Hls();
-  hls.loadSource(document.querySelector("#Player > source").src);
+  hls.loadSource(document.querySelector("video > source").src);
   hls.attachMedia(video);
   hls.on(Hls.Events.MANIFEST_PARSED, function () {
     console.log("Ready to play!");
